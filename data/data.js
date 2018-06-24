29cfd0f88b6da8e91701308be7a265ad766b04d3
osycon <fredrichawk@gmail.com>
2018-06-24 19:07:41 +0200

    Added fake data generation with faker.

--- data/data.js
+++ data/data.js
@@ -1,3 +1,67 @@
+import faker from "faker";
+
+const generateData = (type, seed) => {
+  faker.seed(seed);
+  if (type === "recipe") {
+    return {
+      id: faker.random.uuid(),
+      title: faker.lorem.sentence(),
+      description: faker.lorem.sentence(),
+      cooktime: faker.random.number(99),
+      createdAt: faker.date.past(),
+      ingredients: [
+        {
+          name: faker.random.words(2),
+          amount: faker.random.number(500),
+          unit: faker.random.word()
+        },
+        {
+          name: faker.random.words(2),
+          amount: faker.random.number(500),
+          unit: faker.random.word()
+        },
+        {
+          name: faker.random.words(2),
+          amount: faker.random.number(500),
+          unit: faker.random.word()
+        }
+      ],
+      steps: [
+        faker.random.words(3),
+        faker.random.words(5),
+        faker.random.words(6),
+        faker.random.words(3),
+        faker.random.words(4)
+      ],
+      author: faker.random.uuid()
+    };
+  }
+  if (type === "user") {
+    return {
+      id: faker.random.uuid(),
+      name: faker.name.findName(),
+      email: faker.internet.email(),
+      password: faker.internet.password()
+    };
+  }
+};
+
+const seeds = [155, 324, 173, 321];
+export const data = {
+  User: () => {
+    const users = [...Array(seeds.length)].map((item, index) =>
+      generateData("user", seeds[index])
+    );
+    return users;
+  },
+  Recipe: () => {
+    const recipes = [...Array(seeds.length)].map((item, index) =>
+      generateData("recipe", seeds[index])
+    );
+    return recipes;
+  }
+};
+
 export const users = [
   {
     id: 1231,
