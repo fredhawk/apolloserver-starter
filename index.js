8939f62cb38a6577f89d3e458c72c9df61fcbe2a
osycon <fredrichawk@gmail.com>
2018-07-10 19:58:09 +0200

    Added connection to MongoDB.

--- index.js
+++ index.js
@@ -1,7 +1,22 @@
 import { ApolloServer } from "apollo-server";
+import mongoose from "mongoose";
+
 import typeDefs from "./schemas/schema";
 import resolvers from "./resolvers/resolvers";
 import mocks from "./mocks/mocks";
+
+import "dotenv/config";
+
+// Setup MongoDB connection
+mongoose.connect(
+  process.env.MONGODB,
+  { useNewUrlParser: true }
+);
+const db = mongoose.connection;
+db.on(`error`, console.error.bind(console, "MongoDB connection error:"));
+db.once(`open`, () => {
+  console.log("Connected to MongoDB");
+});
 
 // In the most basic sense, the ApolloServer can be started
 // by passing type definitions (typeDefs) and the resolvers
