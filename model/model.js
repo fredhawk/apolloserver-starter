4b981216a9fbf1de1e3b2a48c6feea2333b6158f
osycon <fredrichawk@gmail.com>
2018-07-10 19:58:32 +0200

    Added Initial Mongoose model.

---
+++ model/model.js
@@ -0,0 +1,29 @@
+import mongoose from "mongoose";
+
+const { Schema } = mongoose;
+
+const userSchema = new Schema({
+  name: String,
+  email: String,
+  password: String
+});
+
+const recipeSchema = new Schema({
+  title: String,
+  description: String,
+  cooktime: Number,
+  createdAt: Date,
+  ingredients: [
+    {
+      ingredientname: String,
+      amount: Number,
+      unit: String
+    }
+  ],
+  steps: [String],
+  author: { type: Schema.Types.ObjectId, ref: `User` }
+});
+
+export const User = mongoose.model(`User`, userSchema);
+
+export const Recipe = mongoose.model(`Recipe`, recipeSchema);
