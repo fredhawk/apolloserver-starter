1cb5aa5f80e32ed4f6bb0f3de83b926b334b452c
osycon <fredrichawk@gmail.com>
2018-07-14 11:33:46 +0200

    Added CRUD tests for users.

--- __tests__/users.js
+++ __tests__/users.js
@@ -11,6 +11,19 @@ describe("User", () => {
 
   beforeEach(async done => {
     await db.collections.users.drop();
+    const jane = await new User({
+      name: "Jane",
+      email: "jane@example.com",
+      password: "asfh2899831"
+    });
+    const rick = await new User({
+      name: "Rick",
+      email: "rick@example.com",
+      password: "lkjsaf187"
+    });
+
+    await jane.save();
+    await rick.save();
     done();
   });
 
@@ -26,11 +39,57 @@ describe("User", () => {
     });
 
     await joe.save();
-    // await expect(!joe.isNew).toBe(true);
+    await expect(!joe.isNew).toBe(true);
     await expect(joe).toHaveProperty("_id");
     await expect(joe).toHaveProperty("name");
     await expect(joe).toHaveProperty("email");
     await expect(joe).toHaveProperty("password");
     done();
+  });
+
+  it("should return all users", async () => {
+    const users = await User.find();
+    expect(users).toHaveLength(2);
+    expect(users[0]).toMatchObject({
+      name: "Jane",
+      email: "jane@example.com",
+      password: "asfh2899831"
+    });
+  });
+  it("should return a user with Janes information", async () => {
+    const jane = await User.findOne({ name: "Jane" });
+    expect(jane).toMatchObject({
+      name: "Jane",
+      email: "jane@example.com",
+      password: "asfh2899831"
+    });
+  });
+
+  it("should update a users information", async () => {
+    const rick = await User.findOneAndUpdate(
+      { name: "Rick" },
+      { email: "ricksemail@example.com" },
+      { new: true }
+    );
+    expect(rick).toMatchObject({
+      name: "Rick",
+      email: "ricksemail@example.com"
+    });
+  });
+
+  it("should delete a user.", async () => {
+    const deletedUser = await User.findOneAndRemove({ name: "Jane" });
+    expect(deletedUser).toMatchObject({
+      name: "Jane",
+      email: "jane@example.com",
+      password: "asfh2899831"
+    });
+    const users = await User.find();
+    expect(users).toHaveLength(1);
+    expect(users[0]).toMatchObject({
+      name: "Rick",
+      email: "rick@example.com",
+      password: "lkjsaf187"
+    });
   });
 });
