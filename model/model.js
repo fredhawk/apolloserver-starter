0a53967bc63b6651e44bf329fda4eb3c1980deeb
osycon <fredrichawk@gmail.com>
2018-07-16 13:42:03 +0200

    Updated model with requirements and timestamps.

--- model/model.js
+++ model/model.js
@@ -2,28 +2,65 @@ import mongoose from "mongoose";
 
 const { Schema } = mongoose;
 
-const userSchema = new Schema({
-  name: String,
-  email: String,
-  password: String,
-  recipes: [{ type: Schema.Types.ObjectId, ref: "Recipe" }]
-});
+const userSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: true
+    },
+    email: {
+      type: String,
+      required: true
+    },
+    password: {
+      type: String,
+      required: true
+    },
+    recipes: [{ type: Schema.Types.ObjectId, ref: "Recipe" }]
+  },
+  { timestamps: true }
+);
 
-const recipeSchema = new Schema({
-  title: String,
-  description: String,
-  cooktime: Number,
-  createdAt: Date,
-  ingredients: [
-    {
-      name: String,
-      amount: Number,
-      unit: String
-    }
-  ],
-  steps: [String],
-  author: { type: Schema.Types.ObjectId, ref: `User` }
-});
+const recipeSchema = new Schema(
+  {
+    title: {
+      type: String,
+      required: true
+    },
+    description: {
+      type: String,
+      required: true
+    },
+    cooktime: {
+      type: Number,
+      required: true
+    },
+    ingredients: [
+      {
+        name: {
+          type: String,
+          required: true
+        },
+        amount: {
+          type: Number,
+          required: true
+        },
+        unit: {
+          type: String,
+          required: true
+        }
+      }
+    ],
+    steps: [
+      {
+        type: String,
+        required: true
+      }
+    ],
+    author: { type: Schema.Types.ObjectId, ref: "User" }
+  },
+  { timestamps: true }
+);
 
 export const User = mongoose.model(`User`, userSchema);
 
